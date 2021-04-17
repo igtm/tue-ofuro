2cfc862c06f0dbe6e4e47ce2ab5c89a7cc834f9a
Tomokatsu Iguchi <tomo14916253@gmail.com>
2021-04-17 12:40:19 +0900

    [live]

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { GetStaticProps, NextPage } from "next";
-import Image from "next/image";
+import Head from "next/head";
 import Link from "next/link";
 import { FC } from "react";
 import Parser from "rss-parser";
 import { useFloatingPlayDispatchContext } from "../context/FloatingPlayAreaContext";
 import { isPodcastEpisodes, PodcastEpisode } from "../types";
-import Head from "next/head";
+import { SvgPlayArrow } from "../components/atoms/SvgPlayArrow";
+import { Paragraph } from "../components/atoms/Paragraph";
 
 type Props = {
   episodes: PodcastEpisode[];
@@ -17,29 +18,36 @@ const Page: NextPage<Props> = ({ episodes }) => {
       <Head>
         <title>火曜日のおフロ</title>
       </Head>
-      <main className="w-full">
-        <div className="mb-6">
+      <main className="w-full grid gap-y-8">
+        <div className="grid gap-y-4">
           <h1>火曜日のおフロ</h1>
-          <p>
-            <a href="https://github.com/igtm" target="_blank" rel="noreferrer">
-              @igtm
-            </a>
-            ,
-            <a href="https://github.com/t-gyo" target="_blank" rel="noreferrer">
-              @t-gyo
-            </a>
-            ,
+
+          <Paragraph>
             <a
+              className="underline hover:nounderline"
+              href="https://github.com/igtm"
+              target="_blank"
+              rel="noreferrer"
+            >@igtm</a>
+            ,{" "}
+            <a
+              className="underline hover:nounderline"
+              href="https://github.com/t-gyo"
+              target="_blank"
+              rel="noreferrer"
+            >@t-gyo</a>
+            ,{" "}
+            <a
+              className="underline hover:nounderline"
               href="https://github.com/ymdarake"
               target="_blank"
               rel="noreferrer"
-            >
-              @ymdarake
-            </a>
+            >@ymdarake</a>{" "}
             が、ゆるーくフロントエンド周りの気になった記事を紹介しながらお届けします。おフロは「フロントエンド」から来てます。毎週土曜日更新。
-          </p>
+          </Paragraph>
         </div>
-        <ul>
+
+        <ul className="grid gap-y-8">
           {episodes.map((e) => (
             <PodcastEpisodeListItem key={e.guid} episode={e} />
           ))}
@@ -63,34 +71,40 @@ const PodcastEpisodeListItem: FC<ListItemProps> = (props) => {
 
   return (
     <li>
-      <div className="flex flex-row">
-        <div className="flex-shrink-0 self-center">
-          <Image src="/saru.jpg" width={128} height={128} />
+      <div className="flex gap-4">
+        <div className="flex-shrink-0 relative w-20 h-20 rounded overflow-hidden">
+          <img src="/saru.jpg" width={128} height={128} />
+
+          <button
+            className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-gray-700 bg-opacity-20 hover:bg-opacity-5"
+            onClick={() => {
+              updateFloatingPlayAreaState(props.episode);
+            }}
+          >
+            <SvgPlayArrow className="w-10 h-10 fill-current text-gray-50" />
+            <span className="sr-only">再生</span>
+          </button>
         </div>
-        <div>
-          <Link href={`/${props.episode.guid}`}>
-            <a>{props.episode.title}</a>
-          </Link>
-          <div className="text-gray-500">
-            {new Date(Date.parse(props.episode.pubDate)).toLocaleDateString()}
-          </div>
-          <div className="w-80 m-4 flex flex-col">
-            <button
-              onClick={() => {
-                updateFloatingPlayAreaState(props.episode);
-              }}
-            >
-              play
-            </button>
-            <button
-              onClick={() => {
-                clearFloatingPlayAreaState();
-              }}
-            >
-              pause
-            </button>
-          </div>
-        </div>
+
+        <Link href={`/${props.episode.guid}`}>
+          <a className="flex-grow hover:underline">
+            <div className="grid gap-y-2">
+              <div className="text-xs text-gray-500">
+                {new Date(
+                  Date.parse(props.episode.pubDate)
+                ).toLocaleDateString()}
+              </div>
+
+              <div className="text-base text-gray-900">
+                {props.episode.title}
+              </div>
+
+              <div className="text-xs text-gray-900">
+                {fmtPrettyJPTime(Number(props.episode.itunes.duration))}
+              </div>
+            </div>
+          </a>
+        </Link>
       </div>
     </li>
   );
@@ -130,3 +144,15 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
     },
   };
 };
+
+function fmtPrettyJPTime(sec: number): string {
+  let hour = Math.floor(sec / 3600);
+  let min = `${Math.floor(sec / 60) % 60}`.padStart(2, "0");
+  let secOnly = `${sec % 60}`.padStart(2, "0");
+  let ret = "";
+  if (hour > 0) {
+    ret += `${hour}:`;
+  }
+  ret += `${min}:${secOnly}`;
+  return ret;
+}
