f4c4f6070f161f142c4847199b85a7eea26d3f63
ymdarake <t.sakamoto.ouen@gmail.com>
2021-08-11 19:13:40 +0900

    不要なパッケージコメント

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import { join } from 'path';
-import matter from 'gray-matter';
+// import matter from 'gray-matter';
 import { Post } from '../types';
 
 const postsDirectory = join(process.cwd(), 'posts');
