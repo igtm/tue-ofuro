e914088e1037cc3d39ace675c2dd1369c22c5bb1
gyo <tamura.gyo@gmail.com>
2021-08-11 21:09:27 +0900

    posts ディレクトリ配下を自由な深さにできるようにする

--- lib/api.ts
+++ lib/api.ts
@@ -5,10 +5,6 @@ import { Post } from '../types';
 
 const postsDirectory = join(process.cwd(), 'posts');
 
-export function getPostDirs() {
-  return fs.readdirSync(postsDirectory);
-}
-
 export function getPostBySlug(slug: string[]): Post {
   const fullPath = join(postsDirectory, ...slug);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
@@ -21,11 +17,39 @@ export function getPostBySlug(slug: string[]): Post {
 }
 
 export function getAllPostSlugs() {
-  const dirs = getPostDirs(); // ['10-usability-heuristics']
-  const slugsGroupedByDir = dirs.map((dir) => getPostSlugsByDir(dir));
-  return slugsGroupedByDir.flat();
+  return recursiveReadDirSync(postsDirectory);
 }
 
-function getPostSlugsByDir(dir: string) {
-  return fs.readdirSync(join(postsDirectory, dir)).map((slug) => [dir, slug]);
+/**
+ *
+ * @param searchPath 再帰的に検索する際のルートとなるディレクトリ
+ * @param slugAccumulator 再帰処理のために関数内部で利用する引数。関数を利用するときに指定する必要はない
+ * @param accumulator 再帰処理のために関数内部で利用する引数。関数を利用するときに指定する必要はない
+ * @returns searchPath 配下のファイルのパスを配列で表現したもの。例: `[["file1.name"], ["path1" ,"path2", "file2.name"]]`
+ */
+function recursiveReadDirSync(
+  searchPath: string,
+  slugAccumulator: string[] = [],
+  accumulator: string[][] = []
+) {
+  const currentPath = join(searchPath, ...slugAccumulator);
+
+  const directoryEntries = fs.readdirSync(currentPath, {
+    withFileTypes: true,
+  });
+
+  for (const directoryEntry of directoryEntries) {
+    const directoryEntrySlugs = [...slugAccumulator, directoryEntry.name];
+
+    if (directoryEntry.isFile()) {
+      accumulator?.push(directoryEntrySlugs);
+    }
+
+    if (directoryEntry.isDirectory()) {
+      // ディレクトリの場合は再帰的に処理する
+      recursiveReadDirSync(searchPath, directoryEntrySlugs, accumulator);
+    }
+  }
+
+  return accumulator;
 }
