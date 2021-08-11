0d602477929977131d69e4789c382e5075d2eedc
ymdarake <t.sakamoto.ouen@gmail.com>
2021-08-11 19:21:44 +0900

    [feat]ブログ：仮の一覧ページを作成

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,3 +1,38 @@
-export default function Page() {
-  return <div>Blog</div>;
-}
+import { NextPage } from 'next';
+import Link from 'next/link';
+import path from 'path';
+import { getAllPostSlugs } from '../../lib/api';
+
+type Props = {
+  slugs: string[][];
+};
+
+const Page: NextPage<Props> = ({ slugs }) => {
+  return (
+    <>
+      <ul className="grid gap-y-8">
+        {slugs.map((s) => {
+          const detailPagePath = path.join(...s);
+          return (
+            <li key={detailPagePath}>
+              <Link href={'/blog/' + detailPagePath}>{detailPagePath}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default Page;
+
+export const getStaticProps = async () => {
+  // TODO: postをとってきてtitleにする
+  const slugs = getAllPostSlugs();
+  return {
+    props: {
+      slugs: slugs,
+    },
+    revalidate: 3600,
+  };
+};
